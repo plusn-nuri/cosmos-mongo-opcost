7b22b9f323b1f97d2e6305c7a385d0430068de81
plusn-nuri <nuri@plusnconsulting.com>
2018-04-10 19:09:13 -0700

    package lock included
    command line argument for connection string
    
    Mongo  connection from command line arg or environment variable.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const Server = require('./server');
 const CostChecker = require('./costChecker');
 
-const costChecker = new CostChecker(process.env.COSMOS_CONNECTION_URL);
+const url = process.argv[2] || process.env.COSMOS_CONNECTION_URL;
+
+if(!url){ throw "MongoDB connection url should be specified either:\n  - The first argument to the application\n  - Environment variable COSMOS_CONNECTION_URL"}
+const costChecker = new CostChecker(url);
 
 const server = new Server(costChecker);
 
